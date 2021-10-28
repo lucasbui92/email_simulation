9fa0bef4d7c21d7c9f923450da24cf1e5b731968
Lucas <malygos92ysera@gmail.com>
2021-10-28 10:59:16 +0700

    Registration with Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'users#index'
+
+  devise_for :users
+
+  # resources :users, only: [:destroy]
+  get '/users/sign_out', to: 'devise/sessions#destroy'
 end
